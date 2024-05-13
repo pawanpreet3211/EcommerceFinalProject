24ad63718834419101e0bbcf206ed0748f067194
pawanpreet3211 <pawanpreetkaur968@gmail.com>
2024-05-13 10:40:36 +0500

    Frontend commit 1

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  content: [],
+  theme: {
+    // screens: {
+    //   sm: '480px',
+    //   md: '768px',
+    //   lg: '976px',
+    //   xl: '1440px',
+    // },
+    // colors: {
+    //   'blue': '#1fb6ff',
+    //   'purple': '#7e5bef',
+    //   'pink': '#ff49db',
+    //   'orange': '#ff7849',
+    //   'green': '#13ce66',
+    //   'yellow': '#ffc82c',
+    //   'gray-dark': '#273444',
+    //   'gray': '#8492a6',
+    //   'gray-light': '#d3dce6',
+    // },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
